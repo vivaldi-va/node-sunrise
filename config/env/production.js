86ab93643e0985bc18990b8ab923e7a630c6dba6
Zaccary price <wildtangomedia@gmail.com>
2015-10-05 00:25:02 +0100

    add production config

--- config/env/production.js
+++ config/env/production.js
@@ -1,17 +1,17 @@
 /**
- * Created by Zaccary on 29/09/2015.
+ * Created by Zaccary on 02/10/2015.
  */
 
 module.exports = {
 	"user_id": "3b1029621d5301d7d65fc7e14fea7e3",
-	"rise_duration": 60, // duration of rise in seconds
+	"rise_duration": 60*30, // duration of rise in seconds
 	"check_interval": 1000*60, // interval to check if rise should run
 	temp: {
 		"min": 1600,
 		"max": 20000
 	},
 	brightness: {
-		"max": 200
+		"max": 160
 	},
 	"schedule": {
 		"monday": {hour: 7, minute: 30},
@@ -19,7 +19,7 @@ module.exports = {
 		"wednesday": {hour: 7, minute: 30},
 		"thursday": {hour: 7, minute: 30},
 		"friday": {hour: 7, minute: 30},
-		"saturday": {hour: 7, minute: 30},
-		"sunday": {hour: 7, minute: 30}
+		"saturday": {hour: 10, minute: 0},
+		"sunday": {hour: 10, minute: 0}
 	}
 };
